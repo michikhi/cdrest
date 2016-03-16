3d55ba5b70d9da54b0f17b27a7239d8e203f2e8c
Mimoun Chikhi <mimoun.chikhi@ext.actelion.com>
2016-03-16 23:03:30 +0100

    s

--- src/main/java/sample/auth/handler/UserController.java
+++ src/main/java/sample/auth/handler/UserController.java
@@ -3,8 +3,6 @@ package sample.auth.handler;
 import org.springframework.security.oauth2.config.annotation.web.configuration.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.*;
-
 @EnableResourceServer
 @RestController
 public class UserController {
