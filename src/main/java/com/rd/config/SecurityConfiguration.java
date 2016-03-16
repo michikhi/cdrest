9554d861ea457c11b1704973fea398d44eebcc12
Mimoun Chikhi <mimoun.chikhi@ext.actelion.com>
2016-03-16 23:22:48 +0100

    yes

---
+++ src/main/java/com/rd/config/SecurityConfiguration.java
@@ -0,0 +1,103 @@
+package com.rd.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.ldap.core.support.*;
+import org.springframework.security.access.expression.method.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.authentication.configurers.*;
+import org.springframework.security.config.annotation.method.configuration.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.ldap.*;
+import org.springframework.security.oauth2.provider.expression.*;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new StandardPasswordEncoder();
+//    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
+//
+//    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web
+                .ignoring()
+                .antMatchers("/h2console/**")
+                .antMatchers("/api/register")
+                .antMatchers("/api/activate")
+                .antMatchers("/api/lostpassword")
+                .antMatchers("/api/resetpassword")
+                .antMatchers("/api/hello");
+
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+    private static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
+
+    }
+    @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+//            auth
+//                    .ldapAuthentication()
+//                    .userDnPatterns("uid={0},ou=people")
+//                    .groupSearchBase("ou=groups")
+//                    .contextSource().ldif("classpath:test-server.ldif");
+
+
+            auth.ldapAuthentication()
+                    .userSearchFilter("(cn={0})")
+                    .userSearchBase("ou=users,o=actelion,dc=corp")
+                    .groupSearchBase("ou=rundeck,ou=app,ou=groups,o=actelion,dc=corp")
+                    .groupSearchFilter("(member={0})")
+                    .groupRoleAttribute("cn")
+                    //.rolePrefix("ROLE_")
+                    .contextSource(contextSource());
+
+        }
+
+        @Bean
+        public BaseLdapPathContextSource contextSource() throws Exception {
+            DefaultSpringSecurityContextSource contextSource = new DefaultSpringSecurityContextSource("ldaps://cdpub.actelion.com:636/");
+
+            contextSource.setUserDn("cn=RundeckAdmin,ou=users,o=system");
+            contextSource.setPassword("xnRJUJd4aLy4zu");
+            contextSource.setReferral("ignore");
+            contextSource.afterPropertiesSet();
+
+            return contextSource;
+        }
+    }
+
+}
+
---
+++ src/main/java/com/rd/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.rd.controller;
+
+import org.springframework.security.oauth2.config.annotation.web.configuration.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.*;
+
+@EnableResourceServer
+@RestController
+public class UserController {
+	
+	@RequestMapping("/user")
+	@ResponseBody
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+}
